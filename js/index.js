73e9336e24aea46a5feadcadffc2680d443f748c
RF-code <rfan23@punaou.edu>
2016-06-28 14:29:02 -1000

    Tried out JS some more
    Created a button that changes the color of the background when clicked.
    Also tried adding an animation into the background.

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 var colors = [
-		"red", 
-		"orange", 
-		"yellow", 
-		"green",
-		"blue",
-		"indigo",
-		"violet",
-		];
+		"RED", 
+		"ORANGE", 
+		"YELLOW", 
+		"GREEN",
+		"BLUE",
+		"INDIGO",
+		"VIOLET",
+];
 
 var count = 0;
 
@@ -20,5 +20,15 @@ var cycle = function(array) {
 };
 
 var display = function(){
+	document.body.style.backgroundColor = colors[count]
 	cycle(colors);
 };
+
+var listAll = function(array){
+	for (var i = 0; i < array.length; i++) {
+		document.getElementById('colorSpace').innerHTML +=
+		"<li>" + array[i] + "</li>";
+	}
+}
+
+listAll(colors);
